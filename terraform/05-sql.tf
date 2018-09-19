7cd81c3f0e561086431fa8a7aae65cf7677f4188
Joeri <jvh@jvh.be>
2018-09-19 23:09:16 +0200

    Add internal ip as variable

--- terraform/05-sql.tf
+++ terraform/05-sql.tf
@@ -73,7 +73,7 @@ data "template_file" "sql_ansible" {
 
   vars {
     name      = "${var.PREFIX}-${var.DEPLOYMENTCOLOR}-VM-SQL"
-    arguments = "ansible_host=${data.azurerm_public_ip.cgfpipa.ip_address} ansible_port=8445 gather_facts=no"
+    arguments = "ansible_host=${data.azurerm_public_ip.cgfpipa.ip_address} ansible_port=8445 gather_facts=no internal_host=${azurerm_network_interface.sqlifc.private_ip_address}"
   }
 
   depends_on = ["azurerm_virtual_machine.sqlvm"]
